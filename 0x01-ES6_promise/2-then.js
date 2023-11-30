3e6a08054fecf6820216f69e47ff878ce4dd5424
SafieDeme9 <safiedeme9@gmail.com>
2023-11-29 20:33:51 -0500

    task 2 last correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const objectResolve = { status: 200, body: 'Success' };
-  promise.then(() => objectResolve).catch(() => Error()).finally(() => {
+  promise.then(() => {}).catch(() => Error()).finally(() => {
     console.log('Got a response from the API');
   });
 }
