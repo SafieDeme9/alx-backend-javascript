351c0e7771d9c4e22ccde046292d894abbe38860
SafieDeme9 <safiedeme9@gmail.com>
2023-11-29 20:45:56 -0500

    task 2 last last last correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'Success' })).catch(() => new Error()).finally(() => {
+  // eslint-disable-next-line no-unused-vars
+  promise.then((response) => ({ status: 200, body: 'Success' })).catch((err) => new Error()).finally(() => {
     console.log('Got a response from the API');
   });
 }
